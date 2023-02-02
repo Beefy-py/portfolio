ee7928ee586b3b84d554e4b50a37a92ed35606e8
beefykenny <hoftkenny@gmail.com>
2023-02-02 05:51:36 -0300

    added og meta tags and twitter tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,12 +33,46 @@ const Home: NextPage = () => {
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta
           name="description"
-          content="Explore the portfolio of Kenny Hoft, a software engineer and web developer based in Suriname. Specializing in MERN-stack and NextJS development, Kenny has worked with Tune Creative Studios and Bits Please Technologies. See his latest projects including a Chat App on his portfolio website."
+          content="Explore the portfolio of Kenny Hoft 👨🏽‍💻, a software engineer and web developer based in Suriname. Specializing in MERN-stack and NextJS development, Kenny has worked with Tune Creative Studios and Bits Please Technologies. See his latest projects including a Chat App on his portfolio website."
         />
         <meta
           name="keywords"
           content="kenny hoft,portfolio website,software engineer suriname, web developer suriname,mern-stack developer,nextJS developer, software developer suriname, Tune Creative Studios, Bits Please Technologies, Chat App"
         />
+
+        {/* <!-- HTML Meta Tags --> */}
+        <title>Kenny Hoft -- Software Developer in Suriname</title>
+        <meta
+          name="description"
+          content="Explore the portfolio of Kenny Hoft, a software engineer and web developer based in Suriname. Specializing in MERN-stack and NextJS development, Kenny has worked with Tune Creative Studios and Bits Please Technologies. See his latest projects including a Chat App on his portfolio website."
+        />
+        {/* <!-- Facebook Meta Tags --> */}
+        <meta property="og:url" content="https://kennyhoft.live/" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Kenny Hoft -- Software Developer in Suriname"
+        />
+        <meta
+          property="og:description"
+          content="Explore the portfolio of Kenny Hoft, a software engineer and web developer based in Suriname. Specializing in MERN-stack and NextJS development, Kenny has worked with Tune Creative Studios and Bits Please Technologies. See his latest projects including a Chat App on his portfolio website."
+        />
+        <meta property="og:image" content="/homepage.png" />
+
+        {/* <!-- Twitter Meta Tags --> */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="kennyhoft.live" />
+        <meta property="twitter:url" content="https://kennyhoft.live/" />
+        <meta
+          name="twitter:title"
+          content="Kenny Hoft -- Software Developer in Suriname"
+        />
+        <meta
+          name="twitter:description"
+          content="Explore the portfolio of Kenny Hoft, a software engineer and web developer based in Suriname. Specializing in MERN-stack and NextJS development, Kenny has worked with Tune Creative Studios and Bits Please Technologies. See his latest projects including a Chat App on his portfolio website."
+        />
+        <meta name="twitter:image" content="/homepage.png" />
+
         <meta
           name="google-site-verification"
           content="qC0yuu6ZCdN7M1G07WxH8T_DxKJjJHy83hYmEOnVZss"
